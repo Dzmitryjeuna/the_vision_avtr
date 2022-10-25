68563d5bce0a92cbbae2b966041e50ebdb9be3bf
Diana Shafarenko <110483163+Dzmitryjeuna@users.noreply.github.com>
2022-10-25 15:31:25 +0300

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-$(function(){
-
+$(function () {
+  $('.design-slider').slick({
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true,
+    prevArrow: '<img class="arrow arrow-left" src="assets/img/icons/arrow-left.svg" alt=""></img>',
+    nextArrow: '<img class="arrow arrow-right" src="assets/img/icons/arrow-right.svg" alt=""></img>',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          slidesToShow: 1,
+          variableWidth: false,
+          
+        }
+      }
+    ]
+  });
 });
